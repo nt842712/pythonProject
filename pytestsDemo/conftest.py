20795ea0a81d8d8ee75cf08746b1eca9c060bbc8
Naresh <nareshtalesha03@gmail.com>
2024-03-01 18:15:01 -0600

    test

--- pytestsDemo/conftest.py
+++ pytestsDemo/conftest.py
@@ -6,3 +6,9 @@ def setup():
     print("I will execute first")
     yield
     print("I will execute last")
+
+
+@pytest.fixture()
+def dataload():
+    print("user profile data is being created")
+    return ["Rahul","Shetty","Naresh"]
