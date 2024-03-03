990a668990a331827c946eb7382227cfbfad037a
Naresh <nareshtalesha03@gmail.com>
2024-03-03 16:33:53 -0600

    test

--- upload.py
+++ upload.py
@@ -1,5 +1,6 @@
 import time
 
+import openpyxl
 from selenium.webdriver.chrome import webdriver
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,12 +9,34 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 file_path="/Users/ntalesha/Downloads/download.xlsx"
 
+def update_excel_data(filepath, columnName, fruitName, price):
+    book = openpyxl.load_workbook(filepath)
+    sheet = book.active
+    dic = {}
+    for i in range(1, sheet.max_column + 1):
+        if sheet.cell(row=1, column=i).value == columnName:
+            dic["col"] = i
+
+    for i in range(1, sheet.max_row + 1):
+        for j in range(1, sheet.max_column + 1):
+            if sheet.cell(row=i, column=j).value == fruitName:
+                dic["row"] = i
+
+    sheet.cell(row=dic["row"], column=dic["col"]).value = price
+    book.save(file_path)
+
 driver=webdriver.Chrome()
 driver.implicitly_wait(10)
 driver.get("https://rahulshettyacademy.com/upload-download-test/index.html")
 driver.find_element(By.CSS_SELECTOR,"#downloadButton").click()
 
 #edit excel
+
+
+
+update_excel_data(file_path,"price","Apple","900")
+
+
 
 
 file_input = driver.find_element(By.CSS_SELECTOR,"#fileinput")
@@ -29,3 +52,8 @@ print(priceval)
 
 
 time.sleep(7)
+
+
+
+
+
